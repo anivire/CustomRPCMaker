c63c12577509854cbaec4e5b86df9e58ad1efbbf
Nikita Grichan <56642689+aniv1re@users.noreply.github.com>
2020-08-27 15:20:34 +0300

    File upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace CustomRPCMaker
+{
+    public partial class MainWindow : Window
+    {
+        public bool isChecked = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TaskbarIcon.Icon = new Icon(@"C:\Users\anivire\source\repos\CustomRPCMaker\ui_assets\Discord-Logo-Color.ico");
+            TaskbarIcon.ToolTipText = "Discord RPC Maker";
+        }
+
+        private void CloseApp_Click(object sender, MouseButtonEventArgs e)
+        {
+            Close(); 
+        }
+
+        private void MinimizeApp_Click(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MainWorkingWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var move = sender as System.Windows.Controls.Grid;
+            var win = Window.GetWindow(move);
+            win.DragMove();
+        }
+
+        private void TaskbarIcon_TrayMouseClick(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
+            this.WindowState = WindowState.Normal;
+        }
+
+        private void DetailsButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (isChecked)
+            {
+                isChecked = false;
+                DetailsButton.Source = new BitmapImage(new Uri("C:/Users/anivire/source/repos/CustomRPCMaker/ui_assets/icons/baseline_toggle_on_white_36dp.png"));
+            }
+            else
+            {
+                isChecked = true;
+                DetailsButton.Source = new BitmapImage(new Uri("C:/Users/anivire/source/repos/CustomRPCMaker/ui_assets/icons/baseline_toggle_off_white_36dp.png"));
+            }
+        }
+    }
+}
